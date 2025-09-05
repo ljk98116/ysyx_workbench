0afa3e6a79f81efa7e50ffb3da0edd1bde5e5fcb
ljk98116 <1067046353@qq.com>
2025-09-05 16:19:04 +0800

    fix: fix klib printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,16 +6,141 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char out[2048];
-  memset(out, 0, sizeof(out));
   va_list ap;
-  int ret = -1;
+  int ret = 0;
   va_start(ap, fmt);
-  ret = vsnprintf(out, -1, fmt, ap);
-  va_end(ap);
-  for(int i=0;i<ret;++i){
-    putch(out[i]);
+  while(*fmt) {
+    if (*fmt == '%') {
+      ++fmt;
+      size_t para_len = 0;
+      while (*fmt >= '0' && *fmt <= '9') {
+        para_len = para_len * 10 + (*fmt - '0');
+        ++fmt;
+      }
+      switch (*fmt) {
+        case 'd': {
+          int arg = va_arg(ap, int);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          int n = arg;
+          if(n == 0) val[0] = '0';
+          while (n) {
+            val[cnt++] = n % 10 + '0';
+            n /= 10;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            int has_minus = arg < 0 ? 1 : 0;
+            if (has_minus) {
+              putch('-');
+              ++ret;
+            }
+            for (int i=0;i<para_len - arglen - has_minus;++i) {
+              putch('0');
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            putch(val[i]);
+            ++ret;
+          }
+          break;
+        }
+        case 'u': {
+          uint32_t arg = va_arg(ap, uint32_t);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          if(arg == 0) val[0] = '0';
+          while (arg) {
+            val[cnt++] = arg % 10 + '0';
+            arg /= 10;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            for (int i=0;i<para_len -arglen;++i) {
+              putch('0');
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            putch(val[i]);
+            ++ret;
+          }
+          break;
+        }
+        case 'x': {
+          uint32_t arg = va_arg(ap, uint32_t);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          if(arg == 0) val[0] = '0';
+          while (arg) {
+            val[cnt++] = arg % 16 >= 10 ? (arg % 16 - 10 + 'A') : (arg % 16 + '0');
+            arg /= 16;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            for (int i=0;i<para_len -arglen;++i) {
+              putch('0');
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            putch(val[i]);
+            ++ret;
+          }
+          break;          
+        }
+        case 's': {
+          const char *arg = va_arg(ap, const char *);
+          size_t arglen = strlen(arg);
+          for (int i=0; i<arglen; ++i) {
+            putch(arg[i]);
+            ++ret;
+          }
+          break;
+        }
+        case 'c': {
+          char arg = va_arg(ap, int);
+          putch(arg);
+          ++ret;
+          break;          
+        }
+      }
+      ++fmt; 
+    }
+    else {
+      putch(*fmt);
+      ++fmt;
+      ++ret;
+    }
   }
+  putch('\0');
+  ++ret;
+  va_end(ap);
   return ret;
 }
 
@@ -25,7 +150,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
-  int ret = -1;
+  int ret = 0;
   va_start(ap, fmt);
   ret = vsprintf(out, fmt, ap);
   va_end(ap);
@@ -34,7 +159,7 @@ int sprintf(char *out, const char *fmt, ...) {
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   va_list ap;
-  int ret = -1;
+  int ret = 0;
   va_start(ap, fmt);
   ret = vsnprintf(out, n, fmt, ap);
   va_end(ap);
@@ -42,89 +167,150 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  int len = 0;
-  char buf[2048];
-  while(*fmt != '\0'){
-    switch(*fmt){
-      case '%':{
-        fmt++;
-        memset(buf, 0 , sizeof(buf));
-        /* 统计数字字符串 */
-        int para_len = 0;
-        while((*fmt) >= '0' && (*fmt) <= '9'){
-          para_len = para_len * 10 + (*fmt - '0');
-          fmt++;
-        }
-        switch(*fmt){
-          case 'd':{
-            int val = va_arg(ap, int);
-            if(para_len > 0){
-              int val_len = klib_itoa(val, buf);
-              int off = 0;
-              if(para_len > val_len){
-                for(int i=0;i<para_len - val_len;++i){
-                  *(out + len + i) = '0';
-                }
-                off = para_len - val_len;
-              }
-              memcpy(out + len + off, buf, val_len);
-              len += off + val_len;
-            }
-            else{
-              int val_len = klib_itoa(val, buf);
-              memcpy(out + len, buf, val_len);
-              len += val_len;
-            }           
-            break;
-          }
-          case 's':{
-            char *s = va_arg(ap, char*);
-            memcpy(out + len, s, strlen(s));
-            len += strlen(s);
-            break;
-          }
-          case 'x':{
-            int val = va_arg(ap, int);
-            if(para_len > 0){
-              int val_len = itox(val, buf);
-              int off = 0;
-              if(para_len > val_len){
-                for(int i=0;i<para_len - val_len;++i){
-                  *(out + len + i) = '0';
-                }
-                off = para_len - val_len;
-              }
-              memcpy(out + len + off, buf, val_len);
-              len += off + val_len;
-            }
-            else{
-              int val_len = itox(val, buf);
-              memcpy(out + len, buf, val_len);
-              len += val_len;
-            }           
-            break;            
-          }
-          case 'c':{
-            char val = va_arg(ap, int);
-            *(out + len) = val;
-            ++len;
-            break;
-          }
-        }
-        break;
+  int ret = 0;
+  while(*fmt != '\0'&& ret < n-2){
+    if (*fmt == '%') {
+      ++fmt;
+      size_t para_len = 0;
+      while (*fmt >= '0' && *fmt <= '9') {
+        para_len = para_len * 10 + (*fmt - '0');
+        ++fmt;
       }
-      default: 
-      {
-        *(out + len) = *fmt;
-        ++len;
-        break;
+      switch (*fmt) {
+        case 'd': {
+          int arg = va_arg(ap, int);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          int n = arg;
+          if(n == 0) val[0] = '0';
+          while (n) {
+            val[cnt++] = n % 10 + '0';
+            n /= 10;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            int has_minus = arg < 0 ? 1 : 0;
+            if (has_minus) {
+              if (ret >= n - 2) goto end;
+              *(out + ret) = '-';
+              ++ret;
+            }
+            for (int i=0;i<para_len - arglen - has_minus;++i) {
+              if (ret >= n - 2) goto end;
+              *(out + ret) = '0';
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            if (ret >= n - 2) goto end;
+            *(out + ret) = val[i];
+            ++ret;
+          }
+          break;
+        }
+        case 'u': {
+          uint32_t arg = va_arg(ap, uint32_t);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          if(arg == 0) val[0] = '0';
+          while (arg) {
+            val[cnt++] = arg % 10 + '0';
+            arg /= 10;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            for (int i=0;i<para_len -arglen;++i) {
+              if (ret >= n - 2) goto end;
+              *(out + ret) = '0';
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            if (ret >= n - 2) goto end;
+            *(out + ret) = val[i];
+            ++ret;
+          }
+          break;
+        }
+        case 'x': {
+          uint32_t arg = va_arg(ap, uint32_t);
+          /* 倒序 */
+          char val[32];
+          memset(val, 0, sizeof(val));
+          int cnt = 0;
+          if(arg == 0) val[0] = '0';
+          while (arg) {
+            val[cnt++] = arg % 16 >= 10 ? (arg % 16 - 10 + 'A') : (arg % 16 + '0');
+            arg /= 16;
+          }
+          size_t arglen = strlen(val);
+          /* 翻转 */
+          for (int i=0; i< arglen / 2; ++i) {
+            char tmp = val[i];
+            val[i] = val[arglen - i - 1];
+            val[arglen - i - 1] = tmp;
+          }
+          if (arglen < para_len && para_len > 0) {
+            for (int i=0;i<para_len -arglen;++i) {
+              if (ret >= n - 2) goto end;
+              *(out + ret) = '0';
+              ++ret;
+            }
+          }
+          for(int i=0;i<arglen;++i) {
+            if (ret >= n - 2) goto end;
+            *(out + ret) = val[i];
+            ++ret;
+          }
+          break;          
+        }
+        case 's': {
+          const char *arg = va_arg(ap, const char *);
+          size_t arglen = strlen(arg);
+          for (int i=0; i<arglen; ++i) {
+            if (ret >= n - 2) goto end;
+            *(out + ret) = arg[i];
+            ++ret;
+          }
+          break;
+        }
+        case 'c': {
+          char arg = va_arg(ap, int);
+          if (ret >= n - 2) goto end;
+          *(out + ret) = arg;
+          ++ret;
+          break;          
+        }
       }
+      ++fmt;      
     }
-    fmt++;
+    else {
+      if (ret >= n - 2) goto end;
+      *(out + ret) = *fmt;
+      ++fmt;
+      ++ret;
+    }
   }
-  *(out + len) = *fmt;
-  ++len;
-  return len;
+end:
+  *(out + ret) = '\0';
+  ++ret;
+  return ret;
 }
 
 
